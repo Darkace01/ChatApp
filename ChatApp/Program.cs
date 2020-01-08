d6c9d2d5c93f53b6e41ecd632044d78611735f1b
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-01-08 12:09:22 +0100

    Changing the port or url

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -19,6 +19,7 @@ namespace ChatApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseUrls("http://localhost:2000")
                 .UseStartup<Startup>();
     }
 }
