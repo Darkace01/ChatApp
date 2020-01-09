774a4be6352041b55b202faf2b5e4ba3a91a831d
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-01-09 10:58:16 +0100

    create room button

--- ChatApp/wwwroot/js/site.js
+++ ChatApp/wwwroot/js/site.js
@@ -2,8 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var createRoomBtn = document.getElementById('create-room')
+var createRoomBtn = document.getElementById('create-room-btn')
+var createRooModal = document.getElementById('create-room-modal')
 
 createRoomBtn.addEventListener('click', function(){
-    
+    createRooModal.classList.add('active')
 })
+
+function closeModal(){
+    createRooModal.classList.remove('active')
+}
