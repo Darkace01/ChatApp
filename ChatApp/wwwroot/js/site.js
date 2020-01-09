666aaa1868d4eda313219a1e3907b72fa7db8d6b
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-01-09 10:43:38 +0100

    Create Chat Room Styling

--- ChatApp/wwwroot/js/site.js
+++ ChatApp/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var createRoomBtn = document.getElementById('create-room')
+
+createRoomBtn.addEventListener('click', function(){
+    
+})
