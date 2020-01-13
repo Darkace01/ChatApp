aab140a2730f0401751b95d1cbe360dc88d0b8dc
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-01-13 10:19:25 +0100

    Primary Key for the ChatUser Declared

--- ChatApp/Data/ApplicationDbContext.cs
+++ ChatApp/Data/ApplicationDbContext.cs
@@ -16,5 +16,11 @@ namespace ChatApp.Data
         public DbSet<Chat> Chats { get; set; }
         public DbSet<Message> Messages { get; set; }
         public DbSet<ChatUser> ChatUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            base.OnModelCreating(builder);
+            builder.Entity<ChatUser>()
+            .HasKey(x => new {x.ChatId , x.UserId});
+        }
     }
 }
