1560ba966c17812e201ff2e4324282888549e2cb
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-01-15 13:53:25 +0100

    Chat Design

--- ChatApp/Models/User.cs
+++ ChatApp/Models/User.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 namespace ChatApp.Models
 {
     public class User : IdentityUser{
+        public string ImgUrl {get;set;}
         public ICollection<ChatUser> Chats { get; set; }
     }
 }
